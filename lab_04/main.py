d5a406c40d2fe207c8e8704d88f40521be5dfdee
migregal <devmigregal@gmail.com>
2021-04-09 15:19:27 +0300

    Plotting added

--- lab_04/main.py
+++ lab_04/main.py
@@ -1,7 +1,29 @@
 from sys import argv
 
+import matplotlib.pyplot as plt
+
 from utils import *
 from meansquare import *
+
+
+def plot(dots: list[Dot], approx: list[Dot]) -> None:
+    x, y = [p.x for p in dots], [p.y for p in dots]
+    a, b = min(x), max(x)
+    x_arg = np.linspace(a, b)
+
+    plt.clf()
+
+    plt.title("Approximation using meansquare method")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.grid(which='minor', color='k', linestyle=':')
+    plt.grid(which='major', color='k')
+
+    plt.plot(x, y, "xk")
+
+    plt.plot([p.x for p in approx], [p.y for p in approx])
+
+    plt.show()
 
 def main():
 
@@ -15,10 +37,7 @@ def main():
 
     slae = SLAE().build(dots, deg).solve()
 
-    for i in slae:
-        print(i)
-
-    res = Approx().get_coeffs(slae).build(dots)
+    plot(dots, Approx().get_coeffs(slae).build(dots))
 
 
 if __name__ == "__main__":
