5225a4beb25e9846c52491a3c8deb6b62f978ee7
migregal <devmigregal@gmail.com>
2021-04-09 23:26:49 +0300

    Class instance initializing updated

--- lab_04/meansquare.py
+++ lab_04/meansquare.py
@@ -9,6 +9,8 @@ class Dot(object):
     weight: float
 
     def __init__(self, _x: float, _y: float, _w: float):
+        super.__init__()
+
         self.x, self.y, self.weight = _x, _y, _w
 
 
@@ -25,7 +27,9 @@ class SLAE(object):
                 slae_coeffs = 0.0
                 expanded_coeff = 0.0
                 for k in range(len(ds)):
-                    slae_coeffs += ds[k].weight * (ds[k].x ** i) * (ds[k].x ** j)
+                    slae_coeffs += ds[k].weight * \
+                                   (ds[k].x ** i) * \
+                                   (ds[k].x ** j)
                     expanded_coeff += ds[k].weight * ds[k].y * (ds[k].x ** i)
 
                 self.mat[i][j] = slae_coeffs
@@ -53,6 +57,8 @@ class SLAE(object):
 
 class Approx(object):
     def __init__(self):
+        super.__init__()
+
         self.coeffs = []
 
     def get_coeffs(self, mat: list[list[float]]) -> Approx:
